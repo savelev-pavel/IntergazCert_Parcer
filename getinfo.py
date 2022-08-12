c7007f27385080004f949709d19029895001e759
pavel.savelev <pal.igory4@gmail.com>
2022-08-12 15:33:20 +0300

    Divided in two modules. Added - certificate information collecting. Progress notification.

---
+++ getinfo.py
@@ -0,0 +1,66 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'Accept': '*/*', 'Connection': 'keep-alive', 'User-Agent': 'Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36 OPR/56.0.3051.104'}
+
+def getExpertData(experts_url = 'https://www.intergazcert.ru/register/members/certification-experts/',
+                  expert_number_of_lines=414):
+    global experts_quantity
+    experts_quantity = expert_number_of_lines
+    response = requests.get(experts_url, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    search_lines = ['tr-'+str(i) for i in range(1, expert_number_of_lines + 1)]
+
+    for i in search_lines:
+        line1 = soup.find('tbody')
+        line2 = line1.find(class_=i)
+        if line2 == None:
+            experts_quantity = search_lines.index(i)
+            break
+        name = line2.find(class_='td-0').text
+        date_since = line2.find(class_='td-1').text
+        date_until = line2.find(class_='td-2').text
+        sphere = line2.find(class_='td-3').text
+        okpd_codes = line2.find(class_='td-4').text
+        department = line2.find(class_='td-5').text
+        extra = line2.find(class_='td-6').text
+        yield name,date_since, date_until, sphere, okpd_codes, department, extra
+
+def getCertificateData(certificate_url = 'https://www.intergazcert.ru/register/certificates/active/products/',
+                       cert_number_of_lines=2000):
+    global cert_quantity
+    cert_quantity = cert_number_of_lines
+    response = requests.get(certificate_url, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    search_lines = ['tr-'+str(i) for i in range(1, cert_number_of_lines + 1)]
+
+    for i in search_lines:
+        line1 = soup.find('tbody')
+        line2 = line1.find(class_=i)
+        if line2 == None:
+            сert_quantity = search_lines.index(i)
+            break
+
+        cert_no = line2.find(class_='td-0').text
+        cert_blank = line2.find(class_='td-1').text
+        cert_holder_name = line2.find(class_='td-2').text
+        cert_holder_adress = line2.find(class_='td-3').text
+        cert_holder_contacts = line2.find(class_='td-4').text
+        cert_producer_name = line2.find(class_='td-5').text
+        cert_producer_adress = line2.find(class_='td-6').text
+        cert_producer_contacts = line2.find(class_='td-7').text
+        cert_product_name = line2.find(class_='td-8').text
+        cert_date_since = line2.find(class_='td-9').text
+        cert_date_until = line2.find(class_='td-10').text
+        cert_klass = line2.find(class_='td-11').text
+        cert_scheme = line2.find(class_='td-12').text
+        cert_givenby = line2.find(class_='td-13').text
+        cert_dept = line2.find(class_='td-14').text
+        cert_extra = line2.find(class_='td-15').text
+
+        yield cert_no, cert_blank, cert_holder_name, cert_holder_adress, \
+               cert_holder_contacts, cert_producer_name, cert_producer_adress, \
+               cert_producer_contacts, cert_product_name, cert_date_since, \
+               cert_date_until, cert_klass, cert_scheme, cert_givenby, cert_dept, cert_extra
--- main.py
+++ main.py
@@ -1,37 +1,73 @@
-"""Собираем данные об экспертах в системе добровольной сертификации ИНТЕРГАЗСЕРТ
-    https://www.intergazcert.ru"""
+"""
+Собираем данные об экспертах в системе добровольной сертификации ИНТЕРГАЗСЕРТ
+https://www.intergazcert.ru
+Вдохновлялся этим видео с youtube:
 
-import requests, xlsxwriter
-from bs4 import BeautifulSoup
+"""
 
-headers = {'Accept': '*/*', 'Connection': 'keep-alive', 'User-Agent': 'Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36 OPR/56.0.3051.104'}
+import getinfo, xlsxwriter
 
-page_url = 'https://www.intergazcert.ru/register/members/certification-experts/'
-search_lines = []
+def certificateWriter(certificate_info):
+    book = xlsxwriter.Workbook(r'Intergazcert.xlsx')
+    page = book.add_worksheet('Certificates')
 
-def getExpertData(number_of_lines):
-    response = requests.get(page_url, headers=headers)
-    soup = BeautifulSoup(response.text, 'lxml')
+    page.set_column('A:A', 20)
+    page.set_column('B:B', 20)
+    page.set_column('C:C', 40)
+    page.set_column('D:D', 40)
+    page.set_column('E:E', 20)
+    page.set_column('F:F', 20)
+    page.set_column('G:G', 20)
+    page.set_column('H:H', 20)
+    page.set_column('I:I', 40)
+    page.set_column('J:J', 10)
+    page.set_column('K:K', 10)
+    page.set_column('L:L', 5)
+    page.set_column('M:M', 5)
+    page.set_column('N:N', 40)
+    page.set_column('O:O', 30)
+    page.set_column('P:P', 20)
 
-    search_lines = ['tr-'+str(i) for i in range(1, number_of_lines + 1)]
+    page.write(0,0,'№ сертификата')
+    page.write(0,1,'№ бланка')
+    page.write(0,2,'Заявитель')
+    page.write(0,3,'Адрес')
+    page.write(0,4,'Контакты')
+    page.write(0,5,'Изготовитель')
+    page.write(0,6,'Адрес')
+    page.write(0,7,'Контакты')
+    page.write(0,8,'Продукция')
+    page.write(0,9,'с')
+    page.write(0,10,'по')
+    page.write(0,11,'класс')
+    page.write(0,12,'схема')
+    page.write(0,13,'ОС')
+    page.write(0,14,'ЦОС')
+    page.write(0,15,'Примечание')
 
-    for i in search_lines:
-        line1 = soup.find('tbody')
-        line2 = line1.find(class_=i)
-        if line2 == None: break
-
-        name = line2.find(class_='td-0').text
-        date_since = line2.find(class_='td-1').text
-        date_until = line2.find(class_='td-2').text
-        sphere = line2.find(class_='td-3').text
-        okpd_codes = line2.find(class_='td-4').text
-        department = line2.find(class_='td-5').text
-        extra = line2.find(class_='td-6').text
-
-        #print(f'{name}, аттестат эксперта с {date_since} по {date_until}, область деятельности: {sphere}'
-        #     f'\nкоды ОКПД: {okpd_codes}, \nЦОС: {department},\nПримечание:{extra}\n')
-
-        yield name,date_since, date_until, sphere, okpd_codes, department, extra
+    row = 1
+    column = 0
+    for item in certificate_info:
+        page.write(row, column, item[0])
+        page.write(row, column+1, item[1])
+        page.write(row, column+2, item[2])
+        page.write(row, column+3, item[3])
+        page.write(row, column+4, item[4])
+        page.write(row, column+5, item[5])
+        page.write(row, column+6, item[6])
+        page.write(row, column+7, item[7])
+        page.write(row, column+8, item[8])
+        page.write(row, column+9, item[9])
+        page.write(row, column+10, item[10])
+        page.write(row, column+11, item[11])
+        page.write(row, column+12, item[12])
+        page.write(row, column+13, item[13])
+        page.write(row, column+14, item[14])
+        page.write(row, column+15, item[15])
+        row+=1
+        completed_percent = int((((row - 1) / getinfo.cert_quantity) * 100))
+        print(f'\r{completed_percent}% completed', end='')
+    book.close()
 
 def expertsWriter(experts_info):
     book = xlsxwriter.Workbook(r'Intergazcert.xlsx')
@@ -48,13 +84,14 @@ def expertsWriter(experts_info):
     page.write(0,0,'ФИО')
     page.write(0,1,'с')
     page.write(0,2,'по')
-    page.write(0,3,'область')
+    page.write(0,3,'Область')
     page.write(0,4,'ОКПД')
     page.write(0,5,'ЦОС')
     page.write(0,6,'Примечание')
 
     row = 1
     column = 0
+    counter = 0
     for item in experts_info:
         page.write(row, column, item[0])
         page.write(row, column+1, item[1])
@@ -64,10 +101,16 @@ def expertsWriter(experts_info):
         page.write(row, column+5, item[5])
         page.write(row, column+6, item[6])
         row+=1
+        completed_percent = int((((row-1) / getinfo.experts_quantity) * 100))
+        print(f'\r{completed_percent}% completed',end='')
     book.close()
 
-expertsWriter(getExpertData(414))          #number of lines to collect
-print('Parcing successful!')
-
+expertsWriter(getinfo.getExpertData(expert_number_of_lines = 500))
+print('\r100% completed', end='')
+print('\nИнформация об экспертах сохранена!')
+certificateWriter(getinfo.getCertificateData(cert_number_of_lines = 2000))
+print('\r100% completed', end='')
+print('\nИнформация о действующих сертификатах сохранена!')
+print('COMPLETED')
 
 
